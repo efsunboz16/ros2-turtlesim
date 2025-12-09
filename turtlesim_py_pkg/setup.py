395787a3fd5e5cf6b8de594b6b183cb944dbd32e
efsunboz16 <devrim.boz16@gmail.com>
2025-12-09 21:32:09 +0300

    ros2 turtlesim

---
+++ turtlesim_py_pkg/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'turtlesim_py_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='efsun',
+    maintainer_email='efsun@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            "vel_controller = turtlesim_py_pkg.vel_controller:main" 
+        ],
+    },
+)
